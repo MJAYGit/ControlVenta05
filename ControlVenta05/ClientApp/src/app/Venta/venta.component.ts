882f12c4bc624dde0023d9930d7d853fffe30dc5
Martin <martin.arteaga.trb.977786200@gmail.com>
2023-03-01 14:36:08 -0500

    ok

--- ControlVenta05/ClientApp/src/app/Venta/venta.component.ts
+++ ControlVenta05/ClientApp/src/app/Venta/venta.component.ts
@@ -1,11 +1,12 @@
-import { Component, Inject, Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Component, Inject, Injectable} from '@angular/core';
 import { forkJoin, throwError, pipe, BehaviorSubject, Observable, from } from 'rxjs';
 import { tap, catchError, flatMap } from 'rxjs/operators';
 import { Venta } from '../models/venta';
 import { Autorizacion } from '../models/autorizacion';
 import { VentaService } from '../services/venta.service';
 import { AutorizacionService } from '../services/autorizacion.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-venta-component',
@@ -14,23 +15,37 @@ import { AutorizacionService } from '../services/autorizacion.service';
 
 @Injectable({ providedIn: 'root' })
 
-
 export class VentaComponent {
 
   public vVentasCli: Venta[];
   public vAutorizacion: Autorizacion;
   private auth_token: string = "";
   public xpArray: number[] = [1];
-  public sHost: string = null;
-  public sPuerto: string = null;
   public bCarga = false;
   public sError: string = "";
+  public loginForm!: FormGroup;
 
-  constructor(private aVentaService: VentaService, private aAutorizacionService: AutorizacionService) {
-    this.sHost = "localhost";
+  constructor(private aVentaService: VentaService
+            , private aAutorizacionService: AutorizacionService
+            , public fb: FormBuilder) {
+    this.CrearFormulario();    
   }
-
+  
+  CrearFormulario(): void {
+    this.loginForm = this.fb.group({
+      FormTxtHost: ['localhost', Validators.required],
+      FormTxtPuerto: ['', Validators.required],
+    })
+  }
+    
   CargarData() {
+    
+    this.loginForm.markAllAsTouched();
+    this.loginForm.updateValueAndValidity();
+    
+    if (!this.loginForm.valid) {
+      return;
+    } 
     this.bCarga = true;
     this.sError = "";
     this.vVentasCli = null;
@@ -59,7 +74,7 @@ export class VentaComponent {
 
     return forkJoin(
       this.xpArray.map(xpItem =>
-        this.aAutorizacionService.ObtenerToken(oAutorizacion, this.sHost, this.sPuerto)
+        this.aAutorizacionService.ObtenerToken(oAutorizacion, this.loginForm.get('FormTxtHost').value, this.loginForm.get('FormTxtPuerto').value)
           .pipe(tap((result: Autorizacion) => this.auth_token = result.token))
       )
     );
@@ -68,11 +83,14 @@ export class VentaComponent {
   ListarVentas() {
     return forkJoin( 
       this.xpArray.map(xpItem =>
-        this.aVentaService.ListarVentas(this.auth_token, this.sHost, this.sPuerto)
+        this.aVentaService.ListarVentas(this.auth_token, this.loginForm.get('FormTxtHost').value, this.loginForm.get('FormTxtPuerto').value)
           .pipe(tap((result) => this.vVentasCli = result))
       ) 
     );
-  } 
+  }
+
+  
+
 }
 
  
